dd511f20dd1f5dc47be0746d55f6463bf4cff0b8
Marek Bastian <marekbas121@gmail.com>
2024-02-16 21:28:21 +0100

    add custom path

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from cryptography.fernet import Fernet
 #     with open("key.key", "wb") as key_file:
 #         key_file.write(key)
 
+def get_path():
+    # print('Please type path to file: ')
+    # path = input('-->')
+    path = 'test.txt'
+    return path
+
 def read_key():
     file = open("key.key", "rb")
     key = file.read()
@@ -14,31 +20,32 @@ def read_key():
 key = read_key()
 fer = Fernet(key)
 
-def read_file():
-    file = open("test.txt", 'rb')
+def read_file(path):
+    file = open(path, 'rb')
     file_bytes = file.read()
     file.close
     return file_bytes
 
-def encrypt_file(file_bytes):
-    file_bytes = read_file() 
+def encrypt_file(file_bytes, path):
+    file_bytes = read_file(path) 
     encrypted_data = fer.encrypt(file_bytes)
-    file = open("test.txt", 'wb')
+    file = open(path, 'wb')
     file.write(encrypted_data)
     file.close()
 
-def decrypt_file(file_bytes):
-    file_bytes = read_file() 
+def decrypt_file(file_bytes, path):
+    file_bytes = read_file(path) 
     decrypted_data = fer.decrypt(file_bytes)
-    file = open("test.txt", 'wb')
+    file = open(path, 'wb')
     file.write(decrypted_data)
     file.close()
 
 def main():
-    file_bytes = read_file() 
+    path = get_path()
+    file_bytes = read_file(path) 
     read_key()
-    read_file()
-    # encrypt_file(file_bytes)
-    decrypt_file(file_bytes)
+    read_file(path)
+    # encrypt_file(file_bytes, path)
+    decrypt_file(file_bytes, path)
 
 main()
