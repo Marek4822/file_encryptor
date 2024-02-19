f1b0b95aa87c13723ab5fd9bc96de87f15b3f656
Marek Bastian <marekbas121@gmail.com>
2024-02-19 18:42:50 +0100

    add encrypt and key function

--- file_encryptor.py
+++ file_encryptor.py
@@ -1,5 +1,7 @@
 import ttkbootstrap as ttk
 from tkinter import filedialog
+from cryptography.fernet import Fernet
+import os
 
 
 class App():
@@ -20,15 +22,12 @@ class File_encryptor(ttk.Frame):
         super().__init__(master, padding=(10, 10))
         self.place(x=0, y=0, relwidth=1, relheight=1)
         self.widgets()
-        # self.filenames = ''
-        self.title_label = ''
 
 
     def widgets(self):
         self.columnconfigure((0, 1, 2, 3), weight=1, uniform='a')
         self.rowconfigure((0, 1, 2, 3, 4, 5), weight=1, uniform='a')
         
-    
         title_label = ttk.Label(self, text='File encryptor', font='arial 30') # title label
         open_bttn = ttk.Button(self, text='Open file', command=lambda: [self.get_files(), self.scrollbar(), self.function_button()]) # button
         key_bttn = ttk.Button(self, text='Open Key', command=lambda: [self.key_button()]) # button
@@ -37,21 +36,21 @@ class File_encryptor(ttk.Frame):
         key_bttn.grid(row=1, column=2, sticky='ewn', columnspan=2, padx=10) # grid button
         title_label.grid(row=0, column=0, sticky='', columnspan=4) # title label
 
-
     def function_button(self):
         global encrypt_bttn, decrypt_bttn
-        encrypt_bttn = ttk.Button(self, text='Encrypt files') # button
+        encrypt_bttn = ttk.Button(self, text='Encrypt files', command=lambda: [self.create_key(), self.encrypt_file()]) # button
         decrypt_bttn = ttk.Button(self, text='Decrypt files') # button
         encrypt_bttn.grid(row=5, column=0, sticky='', columnspan=2) # grid button
         decrypt_bttn.grid(row=5, column=2, sticky='', columnspan=2) # grid button
 
     def key_button(self):
-        create_key_bttn = ttk.Button(self, text='Create key') # button
-        select_key_bttn = ttk.Button(self, text='Select key') # button
+        create_key_bttn = ttk.Button(self, text='Create key', command=lambda: [self.create_key()]) # button
+        select_key_bttn = ttk.Button(self, text='Select key', command=lambda: [self.select_key()]) # button
         create_key_bttn.grid(row=2, column=0, sticky='new', columnspan=2, padx=10) # grid button
         select_key_bttn.grid(row=2, column=2, sticky='new', columnspan=2, padx=10) # grid button
 
         elements = scrollbar_text, scrollbar, encrypt_bttn, decrypt_bttn
+        print(type(elements))
         for element in elements:
             element.destroy()
 
@@ -73,14 +72,57 @@ class File_encryptor(ttk.Frame):
         for filename in self.filenames:
             scrollbar_text.insert('end', f'{filename}\n')
 
-    def read_key():
-        pass
-    
-    def read_file():
-        pass
+    def create_key(self):
+        key_name = 'key_gui.key'
+        current_dir = os.getcwd()
+        if not os.path.exists(key_name):
+            key = Fernet.generate_key()
+            with open(key_name, "wb") as key_file:
+                key_file.write(key)
+            self.fer = Fernet(key)
+            self.key_location = current_dir + key_name
+            print(f'your key is: {key}\nKey location: {self.key_location}')
+        else:
+            print('you already created key!, check your directory')
+            file = open(key_name, "rb")
+            key = file.read()
+            self.fer = Fernet(key)
+            print(f'Your key is loaded!\nKey: {key}')
+        
 
-    def encrypt_file():
-        pass
+    def select_key(self):
+        current_dir = os.getcwd()
+        key = filedialog.askopenfilenames(
+            title='Open a file',
+            initialdir=current_dir,
+            filetypes=(('.key', '.key'),))
+        key = ''.join(key)        
+        file = open(key, "rb")
+        key = file.read()
+        self.fer = Fernet(key)
+        print(f'Your key is loaded!\nKey: {key}')
+       
+    
+    def read_file(self):
+        self.readed_files = []
+        for filename in self.filenames:
+            file = open(filename, 'rb')
+            self.file_bytes = file.read()
+            file.close
+            print(self.file_bytes)
+
+
+    def encrypt_file(self):
+        for filename in self.filenames:
+            file = open(filename, 'rb')
+            file_bytes = file.read()
+            file.close
+            print(filename)
+            encrypted_data = self.fer.encrypt(file_bytes)
+            file = open(filename, 'wb')
+            file.write(encrypted_data)
+            file.close()
+            print(encrypted_data)
 
     def decrypt_file():
         pass
